d12a5dca24101d5da7a09815ee6faf3fb8c88f8c
Edwin Zarate <emzp1994@gmail.com>
2026-08-19 20:52:18 -0500

    indicadores

--- src/app/indicadores/transporte/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const IndicadoresTransporte = dynamic(() => import('@/Paginas/IndicadoresTransporte/index'), { ssr: false });
-
-export default function Page() {
-  return <IndicadoresTransporte />;
-}
